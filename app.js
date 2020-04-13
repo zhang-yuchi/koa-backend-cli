de8040ab5394167bec03644595cd34faaf01720e
zhangyuchi <2669327287@qq.com>
2020-04-13 10:09:20 +0800

    最终更新

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const InitManger = require('./core/init')
 const catchError = require('./middlewares/catchException')
 const app = new koa()
 app.use(catchError)//异常捕获 必须在initCore前面
-InitManger.initCore(app)//现在全局将会挂载app
+InitManger.initCore(app)//现在全局将会挂载app,非中间件的功能将在init中实现,而不是app.js
 app.use(bodyparser())//增加body解析功能
 //-------------自定义中间件----------------
 app.listen(3000)
